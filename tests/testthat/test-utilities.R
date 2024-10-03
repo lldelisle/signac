9f235b088c950062fcae670ebeb198cb7add9b79
Lucille Delisle <lucille.delisle@epfl.ch>
2024-10-03 07:01:25 +0200

    add test with fragment end matching chromosome length

--- tests/testthat/test-utilities.R
+++ tests/testthat/test-utilities.R
@@ -148,6 +148,38 @@ test_that("CreateBWGroup works with 100bp tile", {
   expect_equal(object = bw$score, c(4000, 8000, 4000, 2000, 0, 2000, 0))
 })
 
+test_that("CreateBWGroup works with seqlength equal to final pos", {
+  outdir <- file.path(tempdir(), "createBW2")
+  dir.create(outdir, showWarnings = FALSE)
+  fake.bed.data <- data.frame(
+    seqnames = rep("chr1", 5),
+    start = c(0, 10, 100, 110, 300),
+    end = c(100, 150, 200, 250, 500),
+    cell_name = rep("fake_cell", 5),
+    nb = 1:5
+  )
+  write.table(
+    fake.bed.data, file.path(outdir, "0.bed"),
+    col.names = FALSE, quote = FALSE, sep = "\t",
+    row.names = FALSE
+  )
+  CreateBWGroup(
+    groupNamei = "0",
+    availableChr = "chr1",
+    chromLengths = c("chr1" = 500),
+    tiles = GRanges(seqnames = "chr1", ranges = IRanges(start = seq(1, 499, 100), end = c(seq(100, 500, 100)))),
+    normBy = NULL,
+    tileSize = 100,
+    normMethod = "RC",
+    cutoff = NULL,
+    outdir = outdir
+  )
+  expect_equal(object = length(list.files(outdir)), expected = 2)
+  expect(file.exists(file.path(outdir, "0-TileSize-100-normMethod-rc.bw")), "File does not exist.")
+  bw <- rtracklayer::import.bw(file.path(outdir, "0-TileSize-100-normMethod-rc.bw"))
+  expect_equal(object = bw$score, c(6000, 8000, 2000))
+})
+
 test_that("ExportGroupBW works", {
   outdir <- file.path(tempdir(), "ExportGroupBW")
   dir.create(outdir, showWarnings = FALSE)
