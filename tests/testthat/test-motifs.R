cde5c741a28a0320f06bc11e6792710a4df8fca8
Lucille Delisle <lucille.delisle@epfl.ch>
2024-10-15 14:09:30 +0200

    remove usage of TFBSTools and BSgenome

--- tests/testthat/test-motifs.R
+++ tests/testthat/test-motifs.R
@@ -1,53 +1,11 @@
-pwm <- TFBSTools::PFMatrixList(
-  "MA0030.1" = TFBSTools::PFMatrix(
-    ID = "MA0030.1",
-    name = "FOXF2",
-    matrixClass = "Fork head/winged helix factors", strand = "+",
-    bg = c(A = 0.25, C = 0.25, G = 0.25, T = 0.25),
-    tags = list(
-      family = "FOX", species = "9606",
-      tax_group = "vertebrates", medline = "7957066",
-      tfbs_shape_id = "37", remap_tf_name = "FOXF2"
-    ),
-    profileMatrix = matrix(
-      c(
-        1L, 10L, 17L, 13L, 3L, 7L, 0L, 27L, 27L, 27L, 0L, 27L, 16L, 7L,
-        10L, 7L, 4L, 5L, 11L, 0L, 0L, 0L, 0L, 0L, 25L, 0L, 4L, 4L,
-        7L, 5L, 2L, 5L, 8L, 20L, 0L, 0L, 0L, 0L, 0L, 0L, 2L, 6L,
-        9L, 5L, 4L, 5L, 0L, 0L, 27L, 0L, 0L, 0L, 2L, 0L, 5L, 10L
-      ),
-      byrow = TRUE, nrow = 4,
-      dimnames = list(c("A", "C", "G", "T"))
-    )
-  ),
-  "MA0031.1" = TFBSTools::PFMatrix(
-    ID = "MA0031.1",
-    name = "FOXD1",
-    matrixClass = "Fork head/winged helix factors", strand = "+",
-    bg = c(A = 0.25, C = 0.25, G = 0.25, T = 0.25),
-    tags = list(
-      family = "FOX", species = "9606",
-      tax_group = "vertebrates", medline = "7957066",
-      tfbs_shape_id = "38", remap_tf_name = "FOXD1"
-    ),
-    profileMatrix = matrix(
-      c(
-        1L, 0L, 19L, 20L, 18L, 1L, 20L, 7L,
-        1L, 0L, 1L, 0L, 1L, 18L, 0L, 2L,
-        17L, 0L, 0L, 0L, 1L, 0L, 0L, 3L,
-        1L, 20L, 0L, 0L, 0L, 1L, 0L, 8L
-      ),
-      byrow = TRUE, nrow = 4,
-      dimnames = list(c("A", "C", "G", "T"))
-    )
-  )
-)
+pwm <- readRDS("../testdata/pwm_2motifs.rds")
+genome.fasta <- system.file("extdata", "chr1_start.fa", package = "Signac")
+genome <- Rsamtools::FaFile(genome.fasta)
 
 test_that("AddMotifs works", {
-  library(BSgenome.Hsapiens.UCSC.hg19)
   motif <- AddMotifs(
     atac_small[["peaks"]],
-    BSgenome.Hsapiens.UCSC.hg19,
+    genome,
     pwm,
     verbose = FALSE
   )
@@ -55,7 +13,6 @@ test_that("AddMotifs works", {
 })
 
 test_that("AddMotifs works with fakechr", {
-  library(BSgenome.Hsapiens.UCSC.hg19)
   fpath <- system.file("extdata", "fragments.tsv.gz", package = "Signac")
   fragments <- CreateFragmentObject(fpath)
   features <- suppressWarnings(c(
@@ -79,7 +36,7 @@ test_that("AddMotifs works with fakechr", {
   )
   motif <- AddMotifs.default(
     granges(object)[100:nrow(object)],
-    BSgenome.Hsapiens.UCSC.hg19,
+    genome,
     pwm,
     verbose = FALSE
   )
--- tests/testthat/test-preprocessing.R
+++ tests/testthat/test-preprocessing.R
@@ -105,56 +105,14 @@ test_that("NucleosomeSignal works", {
     expected = c(NaN, NaN, 0.0, 2.5, NaN, NaN)
   )
 })
-pwm <- TFBSTools::PFMatrixList(
-  "MA0030.1" = TFBSTools::PFMatrix(
-    ID = "MA0030.1",
-    name = "FOXF2",
-    matrixClass = "Fork head/winged helix factors", strand = "+",
-    bg = c(A = 0.25, C = 0.25, G = 0.25, T = 0.25),
-    tags = list(
-      family = "FOX", species = "9606",
-      tax_group = "vertebrates", medline = "7957066",
-      tfbs_shape_id = "37", remap_tf_name = "FOXF2"
-    ),
-    profileMatrix = matrix(
-      c(
-        1L, 10L, 17L, 13L, 3L, 7L, 0L, 27L, 27L, 27L, 0L, 27L, 16L, 7L,
-        10L, 7L, 4L, 5L, 11L, 0L, 0L, 0L, 0L, 0L, 25L, 0L, 4L, 4L,
-        7L, 5L, 2L, 5L, 8L, 20L, 0L, 0L, 0L, 0L, 0L, 0L, 2L, 6L,
-        9L, 5L, 4L, 5L, 0L, 0L, 27L, 0L, 0L, 0L, 2L, 0L, 5L, 10L
-      ),
-      byrow = TRUE, nrow = 4,
-      dimnames = list(c("A", "C", "G", "T"))
-    )
-  ),
-  "MA0031.1" = TFBSTools::PFMatrix(
-    ID = "MA0031.1",
-    name = "FOXD1",
-    matrixClass = "Fork head/winged helix factors", strand = "+",
-    bg = c(A = 0.25, C = 0.25, G = 0.25, T = 0.25),
-    tags = list(
-      family = "FOX", species = "9606",
-      tax_group = "vertebrates", medline = "7957066",
-      tfbs_shape_id = "38", remap_tf_name = "FOXD1"
-    ),
-    profileMatrix = matrix(
-      c(
-        1L, 0L, 19L, 20L, 18L, 1L, 20L, 7L,
-        1L, 0L, 1L, 0L, 1L, 18L, 0L, 2L,
-        17L, 0L, 0L, 0L, 1L, 0L, 0L, 3L,
-        1L, 20L, 0L, 0L, 0L, 1L, 0L, 8L
-      ),
-      byrow = TRUE, nrow = 4,
-      dimnames = list(c("A", "C", "G", "T"))
-    )
-  )
-)
 test_that("CreateMotifMatrix works", {
-  library(BSgenome.Hsapiens.UCSC.hg19)
+  pwm <- readRDS("../testdata/pwm_2motifs.rds")
+  genome.fasta <- system.file("extdata", "chr1_start.fa", package = "Signac")
+  genome <- Rsamtools::FaFile(genome.fasta)
   motif.matrix <- CreateMotifMatrix(
     features = granges(atac_small),
     pwm = pwm,
-    genome = BSgenome.Hsapiens.UCSC.hg19
+    genome = genome
   )
   expect_equal(dim(motif.matrix), c(323, 2))
   features <- suppressWarnings(c(
